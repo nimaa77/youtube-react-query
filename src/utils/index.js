8fc6422e885294077e1baa64ed26bf8e332a843f
Nima Arefi <nimaarefi@outlook.com>
2020-10-04 13:14:48 +0330

    feat: add comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
+// dumy way to handle the form
+// DON'T try this at Home, School or anywhere
 const getFormValues = (callBack) => (event) => {
-  event.preventDefault()
-  const name = event.target.name.value
-  const amount = event.target.amount.value
-  const description = event.target.description.value
-  callBack({ name, amount, description })
-}
+  event.preventDefault();
+  const name = event.target.name.value;
+  const amount = event.target.amount.value;
+  const description = event.target.description.value;
+  callBack({ name, amount, description });
+};
 
-export { getFormValues }
+export { getFormValues };
